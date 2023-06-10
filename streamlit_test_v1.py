9bcdf5437a145c28764e3b263032f41d55d39758
Arkaprabha Basu <47769368+Thecoder1012@users.noreply.github.com>
2023-06-10 16:25:42 +0530

    streamlit_test_v1.py

--- streamlit_test_v1.py
+++ streamlit_test_v1.py
@@ -45,7 +45,7 @@ def main():
         frame_count = 0
         while video.isOpened():
             ret, frame = video.read()
-            if frame_count % 2 == 0:
+            if frame_count % 4 == 0:
                 text1 = ''
                 frame_count = frame_count + 1
                 spacer = 10
