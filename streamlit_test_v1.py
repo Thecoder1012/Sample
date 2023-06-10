e9d1e03f5dd4d15f9f5bfdd76ef3d0a27e392500
Arkaprabha Basu <47769368+Thecoder1012@users.noreply.github.com>
2023-06-10 17:08:17 +0530

    streamlit_test_v1.py

--- streamlit_test_v1.py
+++ streamlit_test_v1.py
@@ -137,27 +137,28 @@ def main():
         st.write(int(low), " occured: ",int(low_occr)," times" )
         st.write("Average", avg)
 
+        if video_file != None:
+            if not os.path.isfile(output_path):
+                st.error("Video file not found!")
+            else:
+                # Display the video using Streamlit
+                # print(output_path)
+                # input_path = output_path
+                # output_path = 'enc_op.mp4'
+                # ffmpeg.input(input_path).output(output_path, vcodec='libx264', y='1').run()
+                # # os.system('ffmpeg -y -i {} -vcodec libx264 {}'.format(output_path, 'enc_op.mp4'))
+                # op_file = open(output_path, 'rb')
+                # op_bytes = op_file.read()
+                # st.video(op_bytes)
+                st.download_button('Download Processed File', data = output_video)
+
         video.release()
         output_video.release()
 #         cv2.destroyAllWindows()
 
     
 #     print(video_file)
-    if video_file != None:
-        if not os.path.isfile(output_path):
-            st.error("Video file not found!")
-        else:
-            # Display the video using Streamlit
-            # print(output_path)
-            # input_path = output_path
-            # output_path = 'enc_op.mp4'
-            # ffmpeg.input(input_path).output(output_path, vcodec='libx264', y='1').run()
-            # # os.system('ffmpeg -y -i {} -vcodec libx264 {}'.format(output_path, 'enc_op.mp4'))
-            # op_file = open(output_path, 'rb')
-            # op_bytes = op_file.read()
-            # st.video(op_bytes)
-            st.download_button('Download Processed File', output_video)
-
+    
     
 if __name__ == '__main__':
     main()
