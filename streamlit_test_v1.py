966313094336b9fda6295d353ed824f6b2471db2
Arkaprabha Basu <47769368+Thecoder1012@users.noreply.github.com>
2023-06-10 16:50:44 +0530

    streamlit_test_v1.py

--- streamlit_test_v1.py
+++ streamlit_test_v1.py
@@ -149,10 +149,10 @@ def main():
         else:
             # Display the video using Streamlit
             # print(output_path)
-#             output = 'enc_op.mp4'
-#             inp = output_path
-#             ffmpeg.input(inp).output(output, vcodec='libx264', y='1').run()
-#             os.system('ffmpeg -y -i {} -vcodec libx264 {}'.format(output_path, 'enc_op.mp4'))
+            input_path = output_path
+            output_path = 'enc_op.mp4'
+            ffmpeg.input(input_path).output(output_path, vcodec='libx264', y='1').run()
+            # os.system('ffmpeg -y -i {} -vcodec libx264 {}'.format(output_path, 'enc_op.mp4'))
             op_file = open(output_path, 'rb')
             op_bytes = op_file.read()
             st.video(op_bytes)
