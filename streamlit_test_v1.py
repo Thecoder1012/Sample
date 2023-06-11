d58133bbd340decc659ccdd88a9afe83ada32777
Arkaprabha Basu <47769368+Thecoder1012@users.noreply.github.com>
2023-06-11 21:55:03 +0530

    streamlit_test_v1.py

--- streamlit_test_v1.py
+++ streamlit_test_v1.py
@@ -4,7 +4,8 @@ import easyocr
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import ffmpeg
+import ffmpegcv
+# import ffmpeg
 
 class my_dictionary(dict):
  
@@ -24,32 +25,31 @@ def main():
     st.write('Upload a video file to get the Survay')
 
     # Video file upload
-    # video_file = st.file_uploader('', type=['mp4', 'avi', 'webm', 'mpeg'])
-    video_file = st.file_uploader('')
+    video_file = st.file_uploader('', type=['mp4', 'avi'])
 
     if video_file is not None:
-        # Read video filee
-        st.write("hey")
-        video = cv2.VideoCapture(video_file.name)
-        st.write("tum itna jo")
+        # Read video file
+        video = cv2.VideoCapture(video_file.name, cv2.CAP_FFMPEG)
+        # video = ffmpegcv.VideoCapture(video_file.name)
+        # st.write(video.isOpened())
+        # video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        # video.set(cv2.CAP_PROP_FRAME_HEIGHT, 352)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        
+
         fps = video.get(cv2.CAP_PROP_FPS)
         width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
         output_path = "./output.mp4"  # Replace with the desired output video file path
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can use other codecs as well
         output_video = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-        
+
         reader = easyocr.Reader(['en'], gpu=True)
 
         frame_count = 0
         text_dict = my_dictionary()
         frame_count = 0
-        st.write("Look: ",video.isOpened())
         while video.isOpened():
             ret, frame = video.read()
-            st.write("reading frame\n")
             if frame_count % 10 == 0:
                 text1 = ''
                 frame_count = frame_count + 1
@@ -71,7 +71,7 @@ def main():
                     if detection[1].isdigit():
                         if int(detection[1]) != 0:
                             text = detection[1]
-                            print(text)
+                            # print(text)
 
                         if text in text_dict:
                             text_dict[text] += 1
@@ -94,7 +94,7 @@ def main():
                     frame = cv2.putText(frame, "Exported: 4356,23 M Tonne", (20, spacer + 45), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
                     frame = cv2.putText(frame, "Remarks: 874,34 mt", (20, spacer + 60), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
                     
-                    st.image(frame, channels='BGR', caption='Video Frame')
+                    # st.image(frame, channels='BGR', caption='Video Frame')
                     # st.write('Extracted Text:', text)
                     # st.write('Water Density:', 12345)
                     # st.write('Exported:', 12345)
@@ -105,63 +105,66 @@ def main():
                 frame_count = frame_count + 1
             output_video.write(frame)
         
-        # # Check if there are any two-digit keys in the dictionary
-        # has_two_digit_keys = any(key >= 10 for key in my_dict)
+        if video.isOpened():
+            # # Check if there are any two-digit keys in the dictionary
+            # has_two_digit_keys = any(key >= 10 for key in my_dict)
 
-        # # Filter the dictionary based on the condition
-        # filtered_dict = {key: value for key, value in my_dict.items() if not (key < 10 and has_two_digit_keys)}
+            # # Filter the dictionary based on the condition
+            # filtered_dict = {key: value for key, value in my_dict.items() if not (key < 10 and has_two_digit_keys)}
 
-        max_values = sorted(text_dict.values(), reverse=True)[:2]
-        
-        max_keys = [key for key, value in text_dict.items() if value in max_values]
+            max_values = sorted(text_dict.values(), reverse=True)[:2]
+            
+            max_keys = [key for key, value in text_dict.items() if value in max_values]
 
-        key_list = []
-        for key in max_keys:
-            value = text_dict[key]
-            key_list.append(key)
-            # print("Key:", key, "Value:", value)
+            key_list = []
+            for key in max_keys:
+                value = text_dict[key]
+                key_list.append(key)
+                # print("Key:", key, "Value:", value)
 
-        if len(max_keys) == 2:
-            # top = max(max_keys, key=text_dict.get)
-            # max_keys.remove(top)
-            # low = max_keys[0]
-            top = max(key_list)
-            low = min(key_list)
+            if len(max_keys) == 2:
+                # top = max(max_keys, key=text_dict.get)
+                # max_keys.remove(top)
+                # low = max_keys[0]
+                top = max(key_list)
+                low = min(key_list)
 
-        elif len(max_keys) == 1:
-            top = key_list[0]
-            low = key_list[0]
+            elif len(max_keys) == 1:
+                top = key_list[0]
+                low = key_list[0]
 
-        top_occr = int(text_dict[top])
-        low_occr = int(text_dict[low])
+            top_occr = int(text_dict[top])
+            low_occr = int(text_dict[low])
 
-        st.write("top = ",int(top))
-        st.write("low = ",int(low))
-        avg = ((int(top) * top_occr) + (int(low) * low_occr)) / (top_occr + low_occr)
-        st.write(int(top), " occured: ",int(top_occr)," times" )
-        st.write(int(low), " occured: ",int(low_occr)," times" )
-        st.write("Average", avg)
+            st.write("top = ",int(top))
+            st.write("low = ",int(low))
+            avg = ((int(top) * top_occr) + (int(low) * low_occr)) / (top_occr + low_occr)
+            st.write(int(top), " occured: ",int(top_occr)," times" )
+            st.write(int(low), " occured: ",int(low_occr)," times" )
+            st.write("Average", avg)
 
 
-        video.release()
-        output_video.release()
-#         cv2.destroyAllWindows()
+            video.release()
+            output_video.release()
+            #cv2.destroyAllWindows()
 
-        if video_file != None:
-            if not os.path.isfile(output_path):
-                st.error("Video file not found!")
-            else:
-                # Display the video using Streamlit
-                # print(output_path)
-                # input_path = output_path
-                # output_path = 'enc_op.mp4'
-                # ffmpeg.input(input_path).output(output_path, vcodec='libx264', y='1').run()
-                # # os.system('ffmpeg -y -i {} -vcodec libx264 {}'.format(output_path, 'enc_op.mp4'))
-                op_file = open(output_path, 'rb')
-                # op_bytes = op_file.read()
-                # st.video(op_bytes)
-                st.download_button('Download Processed File', data = op_file)
-#                 os.remove(output_path)
+            if video_file != None:
+                if not os.path.isfile(output_path):
+                    st.error("Video file not found!")
+                else:
+                    # Display the video using Streamlit
+                    # print(output_path)
+                    # input_path = output_path
+                    # output_path = 'enc_op.mp4'
+                    # ffmpeg.input(input_path).output(output_path, vcodec='libx264', y='1').run()
+                    # # os.system('ffmpeg -y -i {} -vcodec libx264 {}'.format(output_path, 'enc_op.mp4'))
+                    op_file = open(output_path, 'rb')
+                    # op_bytes = op_file.read()
+                    # st.video(op_bytes)
+                    st.download_button('Download Processed File', data = op_file)
+                    #os.remove(output_path)
+        else:
+            st.write("Formatting Issue")
 
 
     
